631f45fd3648a9ef80eeddd524b13717779834ed
Mel Benson <mebenson@gmail.com>
2023-10-21 15:56:16 -0500

    redid a bunch of files

---
+++ out/webhelp-responsive/oxygen-webhelp/app/nav-links/json/device_listing_page-d211e42.js
@@ -0,0 +1,1 @@
+oxy_webhelp_navlinks = {"device_listing_page-d211e42":{"topics":[{"title":"enrolling a chromebook","shortdesc":"<p class=\"shortdesc\">this is how you enroll a chromebook into your admin console<\/p>","href":"topics\/enrolling_a_chromebook.html","attributes":{"data-id":"enrolling_a_chromebook"},"menu":{"hasChildren":false},"tocID":"enrolling_a_chromebook-d211e50","topics":[]}]}};
---
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,15 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Core",p:["p1"]},{w:"Services",p:["p1"]},{w:"Device",p:["p2","p3"]},{w:"Listing",p:["p2"]},{w:"Page",p:["p2"]},{w:"Settings",p:["p3"]},{w:"enrolling",p:["p4"]},{w:"a",p:["p4","p7"]},{w:"chromebook",p:["p4"]},{w:"Enrollment",p:["p5"]},{w:"Setting",p:["p5"]},{w:"Organizational",p:["p6"]},{w:"Units",p:["p6"]},{w:"turning",p:["p7"]},{w:"on",p:["p7"]},{w:"service",p:["p7"]},{w:"My",p:["p8"]},{w:"first",p:["p8"]},{w:"topic",p:["p8"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4];
+ph["p2"]=[5, 6, 7];
+ph["p3"]=[5, 8];
+ph["p4"]=[9, 10, 11];
+ph["p5"]=[12, 13];
+ph["p6"]=[14, 15];
+ph["p7"]=[16, 17, 10, 18];
+ph["p8"]=[19, 20, 21];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
