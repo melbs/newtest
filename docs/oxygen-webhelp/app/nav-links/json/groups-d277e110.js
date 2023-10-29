9c42ec7e2c79a115f4ee028b58b75c31074e52e5
Mel Benson <mebenson@gmail.com>
2023-10-29 14:52:04 -0500

    added more files, redid map, image

---
+++ docs/oxygen-webhelp/app/nav-links/json/groups-d277e110.js
@@ -0,0 +1,1 @@
+oxy_webhelp_navlinks = {"groups-d277e110":{"topics":[{"title":"Create a group","shortdesc":"<p class=\"shortdesc\">Create a group<\/p>","href":"topics\/create_a_group.html","attributes":{"data-id":"create_a_group"},"menu":{"hasChildren":false},"tocID":"create_a_group-d277e118","topics":[]}]}};
